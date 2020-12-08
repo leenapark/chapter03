47b982c60199e77302e1215e26952620b1279ab3
leenapark <hrn424@gmail.com>
2020-12-08 22:59:49 +0900

    20201208 수업

---
+++ src/com/javaex/ex05/StringApp.java
@@ -0,0 +1,16 @@
+package com.javaex.ex05;
+
+public class StringApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String str = new String("안녕하세요");
+		System.out.println(str);	//println 주소값을 파라미터로 사용하면 toString() 실행한다
+		//System.out.println(str.toString);
+		
+		
+		
+	}
+
+}

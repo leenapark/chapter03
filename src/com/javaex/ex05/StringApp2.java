96cb4494fc137e58b393741c1229440a84b0de8e
leenapark <hrn424@gmail.com>
2020-12-09 21:32:29 +0900

    20201209 보충 공부

--- src/com/javaex/ex05/StringApp2.java
+++ src/com/javaex/ex05/StringApp2.java
@@ -8,29 +8,34 @@ public class StringApp2 {
 		String a = new String(" abcd");
 		String b = new String(",efg ");
 		
+		
+		System.out.println("==concat===============");
+
 		//concat
 		System.out.println(a.concat(b));
-		System.out.println(a);	//a 변하지 않음
+		System.out.println(a);	//위 출력에서 공백이 사라졌지만 실제 a 변하지 않음
 		
 		a = a.concat(b);
 		System.out.println(a);
 		
+		System.out.println("==trim===============");
 		//trim()
 		a = a.trim();
 		System.out.println(a);
 		System.out.println("__" + a + "__");	//공백 확인용
 		
-		a = a.replace("ab","12,");
+		a = a.replace("ab","12,");	//"1번값","2번값" 1번값을 2번값으로 바꿔라
 		System.out.println(a);
 		
-		System.out.println("=================");
+		System.out.println("==split===============");
 		
 		String[] sArr = a.split(",");
 		for(int i=0; i<sArr.length; i++) {
 			System.out.println(sArr[i]);
 		}
 		
-		
+		System.out.println("==substring===============");
+
 		String str = "Hello java";
 		String result01 = str.substring(2);	//갯수만큼 빠짐
 		System.out.println(result01);
--- src/com/javaex/ex06/WrapperApp.java
+++ src/com/javaex/ex06/WrapperApp.java
@@ -15,23 +15,25 @@ public class WrapperApp {
 		System.out.println(sum2);
 		
 		
-		int num = 123;
+		int num = 123;	//비교 값을 바로 넣어줌
 		
-		Integer i01 = 23;	//박싱
+		Integer i01 = 23;	//박싱	//비교 메모리에 올라감 (= new 문법이랑 같은 형태로 볼 수 있음) 
 		i01.parseInt("12313");	//박싱 메소드 포함
 		
-		int no = n02;	//언박싱
+		int no = n02;	//언박싱	//Integer - int 호환
 		System.out.println(no);
 		
 		//메소드 사용 (Integer 예제)
 		int result = i01.parseInt("12345");
+		System.out.println(result);		//숫자로 값이 변해있음
 		
-		//이상한점
-		//"13579" --> 13579 숫자로 변경
-		
-		Integer no03 = new Integer("131313");
+		//이상한점↓
+		//"13579" --> 13579 숫자로 변경 하고 싶을 때
+		//메모리에 올려야 .parseInt 사용 가능 - Integer 박싱 사용 (int 사용x)
+		Integer no03 = new Integer("131313");	//.parseInt를 사용하려고 의미 없는 메모리를 올려줌
 		int result01 = no03.parseInt("13579");
 		
+		//바로 .parseInt 사용하는 법
 		int result02 = Integer.parseInt("13579");
 		System.out.println(result02);
 		
@@ -39,9 +41,9 @@ public class WrapperApp {
 		//12345 --> "12345"
 		String result03 = String.valueOf(12345);
 		System.out.println(result03);
-		
-		
+		//다른 표현
 		String result04 = ""+12345;
+		System.out.println(result04);
 		
 		
 	}
